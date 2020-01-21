a9ffb31b5b534dab57b37df17fbf2acb0ae61c40
jakubgania <contact@jakubgania.io>
2020-01-21 11:35:07 +0100

    form styles updated in search component

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -6,7 +6,7 @@ import { Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
   form: FormGroup;
