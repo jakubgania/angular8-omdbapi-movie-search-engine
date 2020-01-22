205bed73a1fef90dcca74c56f20f2f3ab41ce3e7
jakubgania <contact@jakubgania.io>
2020-01-22 02:10:49 +0100

    update

--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-    production: true,
+    production: false,
     api_key: 'your_api_key',
     base_url: 'http://www.omdbapi.com'
   };
